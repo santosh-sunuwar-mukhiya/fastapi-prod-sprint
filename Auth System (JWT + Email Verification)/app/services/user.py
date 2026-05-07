165500bcf303d0dbe35310813bf4cc5d021f57fd
Santosh Sunuwar Mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-05-07 22:34:09 +0545

    feat(services): implement user service with auth logic
    - Add add() method for user registration with duplicate email validation
    - Add token() method for login with password verification
    - Implement password hashing and token generation
    - Add error handling for invalid credentials and existing users

--- Auth System (JWT + Email Verification)/app/services/user.py
+++ Auth System (JWT + Email Verification)/app/services/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.user import UserCreate, UserLogin
 from app.db.models import User
-from app.core.security import hash_password, verify_password, create_token
+from app.core.security import decode_token, hash_password, verify_password, create_token
 from sqlalchemy import select
 from fastapi import status, HTTPException
 
@@ -10,6 +10,16 @@ class UserService:
         self.session = session
 
     async def add(self, create_user: UserCreate) -> User:
+        existing_user = await self.session.execute(
+            select(User).where(User.email == create_user.email) # type: ignore
+        )
+
+        if existing_user.scalar():
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail = "Email already Exists."
+            )
+
         user = User(
             username=create_user.username,
             email=create_user.email,
@@ -24,10 +34,10 @@ class UserService:
     
     async def token(self, email: str, password: str):
         credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
         result = await self.session.execute(select(User).where(User.email == email))  # type: ignore
         user = result.scalar()
 
@@ -53,3 +63,60 @@ class UserService:
         )
 
         return user, token
+    
+    async def create_reset_token(self, email: str):
+        result = await self.session.execute(
+            select(User).where(User.email == email) # type: ignore
+        )
+
+        user = result.scalar()
+
+        if user is None:
+            raise HTTPException(404, "User not found")
+
+        token = create_token(
+            data={
+                "reset_password": str(user.id)
+            }
+        )
+
+        print(
+            f"http://localhost:8000/user/reset-password?token={token}"
+        )
+
+        return {
+            "message": "Reset link sent"
+        }
+
+    async def reset_password(
+        self,
+        token: str,
+        new_password: str
+    ):
+        try:
+            payload = decode_token(token)
+            user_id = payload.get("reset_password")
+
+            if user_id is None:
+                raise HTTPException(400, "Invalid token")
+
+        except Exception:
+            raise HTTPException(400, "Invalid token")
+
+        result = await self.session.execute(
+            select(User).where(User.id == user_id)
+        )
+
+        user = result.scalar()
+
+        if user is None:
+            raise HTTPException(404, "User not found")
+
+        user.password_hash = hash_password(new_password)
+
+        await self.session.commit()
+
+        return {
+            "message": "Password updated successfully"
+        }
+        
